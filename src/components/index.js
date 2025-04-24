6919390dbd5798bbb20a73e0e1f92297d56e7952
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-04-24 12:34:34 +0100

    fixed index.js: removed Footer() import

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Banner from "./Banner";
 import SectionBar from "./SectionBar";
 import WelcomeSection from "./WelcomeSection"
-import Footer from "./Footer"
+// import Footer from "./Footer"
 import { DateHook } from "../hooks/DateHook";
 
 export default function Home() {
