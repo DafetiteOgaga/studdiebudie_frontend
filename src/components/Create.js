05785819ab2b036b4e61a9798a411e3d4a512127
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-04-28 21:38:59 +0100

    new Create.js: Create.js component

---
+++ src/components/Create.js
@@ -0,0 +1,20 @@
+
+
+export default function Create() {
+	return (
+		<>
+			<div style={{marginTop: 150}}>
+				<div id="create" className="section">
+					<div className="container">
+						<div className="row">
+							<div className="col-md-12">
+								<h2>Create</h2>
+								<p>Welcome to the Create section! Here you can create and manage your content.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
