fba77ffafd80b55d3f1f26b683f1c08b5da17824
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-05-13 13:35:03 +0100

    fixed TextFileHandler.js: added a callback function to pass data back to the parent

--- src/hooks/fileHandlers/TextFileHandler.js
+++ src/hooks/fileHandlers/TextFileHandler.js
@@ -1,8 +1,9 @@
-const handleTxtFile = (file) => {
+const handleTxtFile = (file, callbackFxn) => {
 	const reader = new FileReader();
 	reader.onload = (e) => {
 		const text = e.target.result;
 		console.log({text});
+		callbackFxn(text);
 	};
 	reader.readAsText(file);
 };
