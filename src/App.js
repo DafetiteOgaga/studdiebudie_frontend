290918da7c3865f6e23e08c0865b801aff698caa
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-05-13 13:42:09 +0100

    fixed App.js: added background color styling

--- src/App.js
+++ src/App.js
@@ -2,26 +2,18 @@ import './App.css';
 import "./statics/css/bootstrap.min.css"
 import "./statics/css/style.css"
 import "./statics/css/responsive.css"
-// import "./statics/css/font-awesome.min.css"
-// import "./statics/css/animate.css"
-// import "./statics/css/custom.css"
-// import "./statics/css/flashy.min.css"
-// import "./statics/css/flaticon.css"
-// import "./statics/css/magnific-popup.css"
-// import "./statics/css/responsiveslides.css"
-// import "./statics/css/timeline.css"
 import Home from './components/index';
 import Header from './components/Header';
 import AppRoutes from './Routes';
 
 function App() {
   return (
-    <>
+    <div className="main_bg">
       <Header />
       <AppRoutes>
         <Home />
       </AppRoutes>
-    </>
+    </div>
   );
 }
 
