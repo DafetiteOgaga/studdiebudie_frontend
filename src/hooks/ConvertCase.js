9e7d764284c474a1768cb89b1433c9b0ceae5bbc
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-05-18 16:41:33 +0100

    fixed ConvertCase.js: added ConvertToSentenceCase()

--- src/hooks/ConvertCase.js
+++ src/hooks/ConvertCase.js
@@ -21,3 +21,25 @@ function ConvertCase(str) {
 	return str.charAt(0).toUpperCase() + str.slice(1)
 }
 export { ConvertCase }
+
+function ConvertToSentenceCase(str) {
+	if (!str) return str
+	if (str.includes('-')) {
+		const strArray = str.split('-')
+		return strArray.map((s) => s.charAt(0).toUpperCase() + s.slice(1)).join(' ')
+	}
+	if (str.includes('_')) {
+		const strArray = str.split('_')
+		return strArray.map((s) => s.charAt(0).toUpperCase() + s.slice(1)).join(' ')
+	}
+	if (str.includes(' ')) {
+		const strArray = str.split(' ')
+		return strArray.map((s) => s.charAt(0).toUpperCase() + s.slice(1)).join(' ')
+	}
+	if (str.includes('/')) {
+		const strArray = str.split('/')
+		return strArray.map((s) => s.charAt(0).toUpperCase() + s.slice(1)).join(' ')
+	}
+	return str.charAt(0).toUpperCase() + str.slice(1)
+}
+export { ConvertToSentenceCase }
