622de8d962455ecbafa245cf8282e9a8e2edbbd7
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-05-22 02:21:55 +0100

    new IsMobile.js: dynamically detect device by its media size

---
+++ src/hooks/IsMobile.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+const useIsMobile = () => {
+	const [isMobile, setIsMobile] = useState(window.matchMedia("(max-width: 767px)").matches);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(max-width: 767px)");
+		const handleResize = () => setIsMobile(mediaQuery.matches);
+
+		mediaQuery.addEventListener("change", handleResize);
+		return () => mediaQuery.removeEventListener("change", handleResize);
+	}, []);
+
+	return isMobile;
+};
+export { useIsMobile };
