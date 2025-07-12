efdc02104f271a9997419a27ec032156814ec02b
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-07-12 11:46:18 +0100

    WelcomeSection.js: changed app name to StuddieBudie

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -8,20 +8,20 @@ export default function WelcomeSection() {
 					<div className="col-md-6 layout_padding_2">
 						<div className="full">
 							<div className="heading_main align_left">
-								<h2><span>Welcome To</span> Dafetite</h2>
+								<h2><span>Welcome To</span> StuddieBudie</h2>
 							</div>
 							<div className="full">
 								<p className="align_left">
-									Welcome to Dafetite, the ultimate platform for conducting exams, quizzes, and tests
+									The ultimate platform for conducting exams, quizzes, and tests
 									with ease and efficiency. Whether you're a student preparing for an exam or an educator
-									creating quizzes, Dafetite makes the process seamless and interactive.
+									creating quizzes, StuddieBudie makes the process seamless and interactive.
 									<br /><br />
 									Our app shuffles questions to create a dynamic, randomized experience for every user,
 									ensuring fairness and uniqueness in every test. With real-time marking and immediate
-									feedback on answers, Dafetite helps you track progress and improve every session.
+									feedback on answers, StuddieBudie helps you track progress and improve every session.
 									<br /><br />
 									Simplify the exam process, conduct interactive quizzes, and get accurate results in no
-									time. Join Dafetite today - where testing meets innovation!
+									time. Join StuddieBudie today - where testing meets innovation!
 								</p>
 							</div>
 						</div>
